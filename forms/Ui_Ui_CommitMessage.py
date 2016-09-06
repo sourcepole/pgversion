42a281be6d78c8978375c4dfad84ee3c68ec84d2
Horst Duester <horst.duester@sourcepole.ch>
2016-09-06 15:29:49 +0200

    combobox for recent commit messages added

--- forms/Ui_Ui_CommitMessage.py
+++ forms/Ui_Ui_CommitMessage.py
@@ -1,33 +1,54 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/hdus/.qgis/python/plugins/pgversion/Ui_CommitMessage.ui'
+# Form implementation generated from reading ui file '/home/hdus/dev/qgis/pgversion-plugin/pgversion/forms/Ui_CommitMessage.ui'
 #
-# Created: Sat Nov 27 14:38:13 2010
-#      by: PyQt4 UI code generator 4.7
+# Created: Tue Sep  6 15:29:13 2016
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
 class Ui_CommitMessage(object):
     def setupUi(self, CommitMessage):
-        CommitMessage.setObjectName("CommitMessage")
+        CommitMessage.setObjectName(_fromUtf8("CommitMessage"))
         CommitMessage.resize(400, 300)
         self.gridLayout = QtGui.QGridLayout(CommitMessage)
-        self.gridLayout.setObjectName("gridLayout")
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.textEdit = QtGui.QTextEdit(CommitMessage)
-        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
         self.gridLayout.addWidget(self.textEdit, 0, 0, 1, 1)
         self.buttonBox = QtGui.QDialogButtonBox(CommitMessage)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.gridLayout.addWidget(self.buttonBox, 3, 0, 1, 1)
+        self.cmbMessages = QtGui.QComboBox(CommitMessage)
+        self.cmbMessages.setObjectName(_fromUtf8("cmbMessages"))
+        self.gridLayout.addWidget(self.cmbMessages, 2, 0, 1, 1)
+        self.label = QtGui.QLabel(CommitMessage)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
 
         self.retranslateUi(CommitMessage)
         QtCore.QMetaObject.connectSlotsByName(CommitMessage)
 
     def retranslateUi(self, CommitMessage):
-        CommitMessage.setWindowTitle(QtGui.QApplication.translate("CommitMessage", "Commit Message", None, QtGui.QApplication.UnicodeUTF8))
+        CommitMessage.setWindowTitle(_translate("CommitMessage", "Commit Message", None))
+        self.label.setText(_translate("CommitMessage", "Recent commit messages:", None))
 
 
 if __name__ == "__main__":
