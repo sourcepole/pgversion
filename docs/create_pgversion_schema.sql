86b859398a4c2e668ff3d25c00c66d52bd8ed159
Horst <horst.duester@sourcepole.ch>
2023-03-14 13:12:26 +0100

    Set to DB revision 2.1.16 and plugin revision 3.3.1

--- docs/create_pgversion_schema.sql
+++ docs/create_pgversion_schema.sql
@@ -56,7 +56,7 @@ CREATE OR REPLACE FUNCTION versions.pgvsrevision ()
 DECLARE
   revision TEXT;
   BEGIN	
-    revision := '2.1.15';
+    revision := '2.1.16';
     RETURN revision;                             
   END;
 $$;
